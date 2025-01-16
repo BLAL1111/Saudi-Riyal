a92b462a1ab6693d0e71dbb9db9b1e048584df49
BLAL1111 <llaaghsthqq@gmail.com>
2025-01-17 01:10:25 +0200

    Add files via upload

---
+++ Riyal.js
@@ -0,0 +1,18 @@
+
+let dollar = document.getElementById('dollar'); // الريال السعودي
+let pound = document.getElementById('pound'); // الجنيه المصري
+
+// تحويل من الريال السعودي إلى الجنيه المصري
+dollar.onkeyup = function () {
+  pound.value = (dollar.value * 13.43 ).toFixed(2); // استخدمت معدل افتراضي (8.20)، يمكنك تغييره حسب الحاجة
+};
+
+// تحويل من الجنيه المصري إلى الريال السعودي
+pound.onkeyup = function () {
+  dollar.value = (pound.value / 13.43).toFixed(2);
+};
+
+// لإغلاق الرسالة الترحيبية
+document.getElementById("closeBtn").addEventListener("click", function () {
+  document.getElementById("overlay").style.display = "none";
+});
