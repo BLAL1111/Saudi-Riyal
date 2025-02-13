747d956ece5e87a41dc0e773078a3f74a0e1d131
BLAL1111 <llaaghsthqq@gmail.com>
2025-02-13 08:02:25 +0300

    Update Riyal.js

--- Riyal.js
+++ Riyal.js
@@ -4,12 +4,12 @@ let pound = document.getElementById('pound'); // الجنيه المصري
 
 // تحويل من الريال السعودي إلى الجنيه المصري
 dollar.onkeyup = function () {
-  pound.value = (dollar.value * 13.43 ).toFixed(2); // استخدمت معدل افتراضي (8.20)، يمكنك تغييره حسب الحاجة
+  pound.value = (dollar.value * 13.50 ).toFixed(2); // استخدمت معدل افتراضي (8.20)، يمكنك تغييره حسب الحاجة
 };
 
 // تحويل من الجنيه المصري إلى الريال السعودي
 pound.onkeyup = function () {
-  dollar.value = (pound.value / 13.43).toFixed(2);
+  dollar.value = (pound.value / 13.50).toFixed(2);
 };
 
 // لإغلاق الرسالة الترحيبية
